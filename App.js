0b51e1cdcf076309a4b4cf70b6c4cf6107684d90
José Alejandro <jalejandro@nbty.cloud>
2021-11-15 16:32:55 -0600

    config on iOS

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {SafeAreaView, View, Image, Button, Text} from 'react-native';
+import {SafeAreaView, View, Image, Button, Text, Platform} from 'react-native';
 import RNBlobUtil from 'react-native-blob-util';
 import FastImage from 'react-native-fast-image';
 
 const uriImage =
   'https://www.techup.co.in/wp-content/uploads/2020/01/techup_logo_72-scaled.jpg';
 
+const urlPdf = 'https://api.brain-gbuniversity.com/documents/Competencias.pdf';
+
 const downloadFile = () => {
   const {config, fs} = RNBlobUtil;
 
   config({
     fileCache: true,
+    path: `${fs.dirs.DownloadDir}/ImagenRandom.jpg`,
     addAndroidDownloads: {
       useDownloadManager: true,
       notification: true,
@@ -22,6 +25,49 @@ const downloadFile = () => {
     .fetch('GET', uriImage)
     .then(res => console.log('Respuesta: ', res))
     .catch(err => console.log('Error: ', err));
+};
+
+const handleDownloadFile = async () => {
+  const DIRS = RNBlobUtil.fs.dirs;
+
+  const filename = 'Competencias.pdf';
+  const FileManager = Platform.select({
+    ios: {
+      getDownloadPath: filename => `${DIRS.CacheDir}/${filename}`,
+      preview: path => RNBlobUtil.ios.openDocument(path),
+    },
+    android: {
+      getDownloadPath: filename => `${DIRS.DownloadDir}/${filename}`,
+      preview: RNBlobUtil.android.actionViewIntent,
+    },
+  });
+
+  const path = FileManager.getDownloadPath(filename);
+
+  try {
+    const fetchConfig = {
+      fileCache: true,
+      path,
+      appendExt: filename.split('.').slice(-1)[0],
+      addAndroidDownloads: {
+        title: filename,
+        path,
+        notification: true,
+        useDownloadManager: true,
+        mediaScannable: true,
+      },
+    };
+
+    this.currentTask = RNBlobUtil.config(fetchConfig).fetch('GET', urlPdf);
+
+    const response = await this.currentTask;
+    const downloadPath = response.path();
+    console.log('Path: ', downloadPath);
+    FileManager.preview(downloadPath);
+    this.currentTask = null;
+  } catch (error) {
+    this.currentTask = null;
+  }
 };
 
 const App = () => {
@@ -46,7 +92,7 @@ const App = () => {
           }}
           resizeMode={FastImage.resizeMode.cover}
         />
-        <Button title="Download image" onPress={downloadFile} />
+        <Button title="Download image" onPress={handleDownloadFile} />
       </View>
     </SafeAreaView>
   );
